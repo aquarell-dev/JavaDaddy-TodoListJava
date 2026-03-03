4fdb3d91e7db1b79d3b84c4973f0f184760d602c
aquarell <aquarell.dev@gmail.com>
2026-03-03 12:17:40 +0300

    feat: remove redundant infra

--- src/main/java/com/aquarell/infrastructure/storage/Shard.java
+++
@@ -1,6 +0,0 @@
-package com.aquarell.infrastructure.storage;
-
-import java.util.HashMap;
-import java.util.concurrent.locks.ReadWriteLock;
-
-public record Shard<K, V>(ReadWriteLock lock, HashMap<K, V> value) {}
